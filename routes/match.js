f20a8f34dc127bf8def0d66a372a68a7020c4424
anonymouspotter <anonymouspotter@protonmail.com>
2023-06-29 15:33:05 +0900

    load_candidate에 이미 선택한 사람 나오지 않게 하기

--- routes/match.js
+++ routes/match.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 // hiki 목록 불러오기 (자영업자 사용 API)
 router.post("/load_candidate_hiki", async (req, res, next) => {
-  const getAllHikiSQL = `SELECT user.uid AS uid, user.id AS id, user.nickname AS nickname, profile.info AS info, profile.study_career AS study_career FROM user, youth_profile AS profile WHERE TYPE = 0 AND user.id = profile.uid`;
+  const body = req.body;
+
+  const id = body.id; // 사업자 id
+
+  const getAllHikiSQL = `SELECT user.uid AS uid, user.id AS id, user.nickname AS nickname, profile.info AS info, profile.study_career AS study_career FROM user, youth_profile AS profile WHERE TYPE = 0 AND user.id = profile.uid ORDER BY RAND() LIMIT 10;`;
 
   const hikis = [];
   const hikiInfo = await new Promise((resolve, reject) => {
@@ -21,7 +25,25 @@ router.post("/load_candidate_hiki", async (req, res, next) => {
   });
 
   for (let user of hikiInfo) {
-    
+    const getMatchSQL = `SELECT * FROM match WHERE hiki_id = '${user.id}' AND ceo_id = '${id}'`;
+    const match = await new Promise((resolve, reject) => {
+      db.query(getMatchSQL, (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) 
+          resolve(true);
+        else {
+          if (res[0]['ceo_choice'] == 0){
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    });
+
+    // 보여주지 않을 사람 건너 뛰기
+    if (!match) 
+      continue;
 
     // add career field
     const getHikiCareerSQL = `SELECT * FROM user, youth_career WHERE user.id = youth_career.uid AND user.id = '${user.id}'`;
@@ -55,7 +77,11 @@ router.post("/load_candidate_hiki", async (req, res, next) => {
 
 // 자영업자 목록 불러오기 (히키 사용 API)
 router.post("/load_candidate_ceo", async (req, res, next) => {
-  const getAllCEOSQL = `SELECT user.id AS id, ceo.name AS name, ceo.number AS number, ceo.intro AS intro, ceo.employee_count AS employee_count, ceo.type AS type, ceo.representative AS representative FROM user, company_profile AS ceo WHERE user.id = ceo.uid`;
+  const body = req.body;
+
+  const id = body.id; // 히키 id
+
+  const getAllCEOSQL = `SELECT user.id AS id, ceo.name AS name, ceo.number AS number, ceo.intro AS intro, ceo.employee_count AS employee_count, ceo.type AS type, ceo.representative AS representative FROM user, company_profile AS ceo WHERE user.id = ceo.uid ORDER BY RAND() LIMIT 10;`;
 
   const ceos = [];
   const ceoInfo = await new Promise((resolve, reject) => {
@@ -71,7 +97,25 @@ router.post("/load_candidate_ceo", async (req, res, next) => {
   // console.log(ceoInfo);
 
   for (let user of ceoInfo) {
-    let ceo = {};
+    const getMatchSQL = `SELECT * FROM match WHERE ceo_id = '${user.id}' AND hiki_id = '${id}'`;
+    const match = await new Promise((resolve, reject) => {
+      db.query(getMatchSQL, (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) 
+          resolve(true);
+        else {
+          if (res[0]['hiki_choice'] == 0){
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    });
+
+    // 보여주지 않을 사람 건너 뛰기
+    if (!match) 
+      continue;
 
     // add works field
     const getCEOWorkSQL = `SELECT * FROM company_employment AS ceo_work WHERE ceo_work.uid = '${user.id}'`;
@@ -84,7 +128,8 @@ router.post("/load_candidate_ceo", async (req, res, next) => {
         resolve(res);
       });
     });
-
+      
+    let ceo = {};
     const result = await new Promise((resolve) => {
       ceo.id = user.id;
       ceo.name = user.name;
